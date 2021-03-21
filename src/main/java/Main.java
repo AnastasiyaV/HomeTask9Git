64d5a067ecf0fcd1d4e27b339ecec682185d905f
AnastasiyaV <anastasiya.vynogradska@hotmail.com>
2021-03-21 20:54:43 +0200

    line4 edited second time in master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is line 3!");
-        System.out.println("this is line 4 edited in master");
+        System.out.println("this is line 4 edited in master2");
         System.out.println("this is line 5");
     }
 }
